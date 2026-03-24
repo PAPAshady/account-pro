3c69ce0a9fc19a16c4827ef9b757f5d5eee14b9a
Nima Zamani <zamani.nima18@gmail.com>
2026-03-24 16:16:02 +0330

    feat: implemented header section.

---
+++ src/components/modules/Header/Header.js
@@ -0,0 +1,85 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { FaBars, FaShoppingCart, FaSearch, FaUser } from 'react-icons/fa';
+
+import Navbar from '@/components/modules/Navbar/Navbar';
+import RoundedContainer from '@/ui/RoundedContainer/RoundedContainer';
+import PrimaryButton from '@/components/modules/PrimaryButton/PrimaryButton';
+
+export default function Header() {
+  return (
+    <div>
+      {/* Mobile Header */}
+      <div className="flex items-center justify-between lg:hidden">
+        <RoundedContainer dir="ltr">
+          <button className="bg-foreground grid size-10.5 cursor-pointer place-content-center p-2.5">
+            <FaBars />
+          </button>
+        </RoundedContainer>
+        <Link href="/">
+          <Image
+            alt="اکانت پرو"
+            width={58}
+            height={58}
+            src="/images/logo/logo.png"
+            className="size-7.5 md:size-10"
+          />
+        </Link>
+        <RoundedContainer>
+          <button className="bg-foreground grid size-10.5 cursor-pointer place-content-center p-2.5">
+            <FaShoppingCart />
+          </button>
+        </RoundedContainer>
+      </div>
+      {/* Desktop Header */}
+      <div className="hidden lg:block">
+        <div className="flex items-center justify-between py-2.5">
+          <Link href="/">
+            <Image
+              width={330}
+              height={115}
+              alt="اکانت پرو - مرجع خرید حساب های قانونی"
+              src="/images/logo/logo-lg.png"
+              className="h-17.5 w-47.5"
+            />
+          </Link>
+          <RoundedContainer>
+            <div className="bg-foreground flex items-center gap-3 py-2.5 ps-3.75 pe-2">
+              <div className="text-end">
+                <p className="text-sm font-bold">۰۹۳۳۱۸۴۴۵۴۹</p>
+                <p className="text-paragraph">پشتیبانی ۲۴ ساعته</p>
+              </div>
+              <Image
+                src="/images/support.png"
+                alt="پشتیبانی"
+                width={48}
+                height={48}
+                className="size-12"
+              />
+            </div>
+          </RoundedContainer>
+        </div>
+        <div className="hidden items-center justify-between gap-3.5 py-2.5 lg:flex">
+          <Navbar />
+          <RoundedContainer
+            containerClassName="grow"
+            className="bg-foreground flex items-center gap-2 px-3.5"
+          >
+            <input type="text" placeholder="جستوجو کنید..." className="h-11.25 grow outline-none" />
+            <FaSearch className="text-paragraph min-h-4 min-w-4 shrink-0" />
+          </RoundedContainer>
+          <PrimaryButton>
+            ورود/ثبت نام
+            <FaUser />
+          </PrimaryButton>
+          <RoundedContainer>
+            <PrimaryButton className="px-3!">
+              <FaShoppingCart />
+            </PrimaryButton>
+          </RoundedContainer>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/modules/Navbar/Navbar.js
@@ -0,0 +1,48 @@
+'use client';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import { FaChevronDown } from 'react-icons/fa';
+import clsx from 'clsx';
+
+import RoundedContainer from '@/ui/RoundedContainer/RoundedContainer';
+
+const navLinks = [
+  { id: 1, title: 'صفحه اصلی', href: '/' },
+  { id: 2, title: 'سرویس ها', href: '/services', hasMenu: true },
+  { id: 4, title: 'پلن ها', href: '/plans' },
+  { id: 5, title: 'وبلاگ', href: '/blog' },
+  { id: 3, title: 'درباره ما', href: '/about-us' },
+  { id: 6, title: 'تماس با ما', href: '/contact-us' },
+];
+
+export default function Navbar() {
+  const pathname = usePathname();
+
+  return (
+    <RoundedContainer containerClassName="grow-2">
+      <nav className="bg-foreground">
+        <ul className="flex items-center">
+          {navLinks.map((navLink) => (
+            <li className="grow" key={navLink.id}>
+              <Link
+                className={clsx(
+                  'flex items-center justify-center gap-2 px-2.5 pt-2.5 pb-3',
+                  pathname.includes(navLink.href) ? 'font-bold text-white' : 'text-paragraph'
+                )}
+                href={navLink.href}
+              >
+                {navLink.title}
+                {navLink.hasMenu && (
+                  <span className="text-primary mt-1 text-xs">
+                    <FaChevronDown />
+                  </span>
+                )}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </RoundedContainer>
+  );
+}
