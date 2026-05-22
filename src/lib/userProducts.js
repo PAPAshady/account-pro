628d29bfa5d4e43b49ceb0fb336f72c3a530d362
Nima Zamani <zamani.nima18@gmail.com>
2026-05-22 16:34:11 +0330

    fix: fixed the case where if user had no products list in db, it would cause an error in /dashboard page.

--- src/lib/userProducts.js
+++ src/lib/userProducts.js
@@ -6,14 +6,20 @@ export const getUserProducts = async () => {
   try {
     await connectToDB();
     const userRes = await validateUser();
-    if (userRes.status !== 200) return null;
+    if (userRes.status !== 200) return [];
     const { user } = await userRes.json();
     const userProducts = await userProductsModel
       .findOne({ user: user._id }, 'products -_id')
       .lean();
+
+    if (!userProducts) {
+      await userProductsModel.create({ user: user._id, products: [] });
+      return [];
+    }
+
     return JSON.parse(JSON.stringify(userProducts.products)) || [];
   } catch (err) {
     console.log('Failed to get user products => ', err);
-    return null;
+    return [];
   }
 };
