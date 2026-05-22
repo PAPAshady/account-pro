ecbf298b5816d54d8b085e19f38036fde923bbe2
Nima Zamani <zamani.nima18@gmail.com>
2026-05-22 18:25:28 +0330

    feat: implemented logout logic and dynmaic user data in dashboard sidebar.

--- src/components/templates/Dashboard/Sidebar/Sidebar.jsx
+++ src/components/templates/Dashboard/Sidebar/Sidebar.jsx
@@ -1,15 +1,36 @@
 'use client';
 import Image from 'next/image';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
 import { FaChevronLeft, FaSignOutAlt } from 'react-icons/fa';
 import clsx from 'clsx';
+import { toast } from 'sonner';
 
+import useAuth from '@/store/useAuth';
 import { dashboardLinks } from '@/data';
+import { USER_ROLES } from '@/constants';
+import api from '@/axiosInstance';
 
 export default function Sidebar() {
+  const router = useRouter();
   const pathname = usePathname();
+  const user = useAuth((state) => state.user);
+  const setUser = useAuth((state) => state.setUser);
+
+  const signOutHandler = async () => {
+    try {
+      const res = await api.post('/api/auth/signout');
+      if (res.status === 200) {
+        setUser(null);
+        router.replace('/');
+        toast.success('از حساب کاربری خود خارج شدید.');
+      }
+    } catch (error) {
+      console.log('Error logging out user => ', error);
+      toast.error('خطا در خروج از حساب کاربری.');
+    }
+  };
 
   return (
     <aside className="hidden h-full w-1/4 lg:block xl:w-62.5">
@@ -17,15 +38,15 @@ export default function Sidebar() {
         <div className="bg-primary bg-hatching rounded-2xl rounded-tr-3xl p-6.25 pb-2.5 text-[#191919]">
           <div>
             <Image
-              alt="نیما زمانی"
+              alt={user?.name || 'User name'}
               width={60}
               height={60}
               className="border-primary -mt-10 size-14 rounded-3xl rounded-tr-lg border-4 object-cover"
               src="/images/profile/profile2.png"
             />
           </div>
-          <p className="my-2 text-xl font-bold">نیما زمانی</p>
-          <p className="font-normal">مشتری</p>
+          <p className="my-2 text-xl font-bold">{user?.name}</p>
+          <p className="font-normal">{user?.role === USER_ROLES.USER ? 'مشتری' : 'ادمین'}</p>
         </div>
         <nav className="bg-foreground rounded-3xl rounded-tr-lg p-5">
           <ul>
@@ -56,14 +77,17 @@ export default function Sidebar() {
               </li>
             ))}
             <li>
-              <button className="group mb-2.5 flex items-center justify-between gap-2 pb-2.5">
+              <button
+                onClick={signOutHandler}
+                className="group mb-2.5 flex w-full cursor-pointer items-center justify-between gap-2 pb-2.5"
+              >
                 <span className="mt-1 flex items-center gap-2">
-                  <i className="group-hover:bg-primary grid size-8 place-content-center rounded-lg rounded-tr-sm bg-[#ffffff1a] transition-colors duration-300 group-hover:text-[#336]">
+                  <i className="grid size-8 place-content-center rounded-lg rounded-tr-sm bg-[#ffffff1a] transition-colors duration-300 group-hover:bg-red-400">
                     <FaSignOutAlt />
                   </i>
                   <span>خروج از حساب کاربری</span>
                 </span>
-                <FaChevronLeft className="text-primary text-sm opacity-0 transition-opacity duration-300 group-hover:opacity-100" />
+                <FaChevronLeft className="text-sm text-red-400 opacity-0 transition-opacity duration-300 group-hover:opacity-100" />
               </button>
             </li>
           </ul>
