1588aa209ade22a7de6b7794f3da9d0cc1f821da
Nima Zamani <zamani.nima18@gmail.com>
2026-05-22 19:18:11 +0330

    feat: implemented empty ui state for licences page in dashboard.

--- src/app/(dashboard)/dashboard/licenses/page.jsx
+++ src/app/(dashboard)/dashboard/licenses/page.jsx
@@ -1,8 +1,11 @@
 import { Suspense } from 'react';
 
+import { FaSearchMinus } from 'react-icons/fa';
+
 import LicenceCard from '@modules/Cards/LicenceCard/LicenceCard';
 import LicenceCardSkeleton from '@modules/Cards/LicenceCard/LicenceCardSkeleton';
 import { getUserProducts } from '@/lib/userProducts';
+import PrimaryButton from '@modules/PrimaryButton/PrimaryButton';
 
 export default async function page() {
   return (
@@ -21,6 +24,23 @@ export default async function page() {
 
 async function LicencesList() {
   const products = await getUserProducts();
+  if (!products.length) {
+    return (
+      <div className="border-primary/30 mt-10 flex flex-col items-center justify-center gap-6 rounded-lg border px-4 py-5 text-center sm:py-10">
+        <FaSearchMinus className="text-2xl" />
+        <div className="space-y-2">
+          <p>در حال حاضر اشتراک فعالی ندارید.</p>
+          <p className="text-paragraph mb-6 text-sm">
+            لطفا محصولات مورد نظر خود را انتخاب و خرید نمایید.
+          </p>
+          <PrimaryButton isLink href="/shop" isHighLight>
+            دیدن محصولات
+          </PrimaryButton>
+        </div>
+      </div>
+    );
+  }
+
   return products.map((product) => <LicenceCard key={product._id} {...product} />);
 }
 
