714cde9f7fcd1ba4d69aede02bf27b7dd790f3dd
Nima Zamani <zamani.nima18@gmail.com>
2026-05-27 13:12:28 +0330

    refactor: removed mock mobile filters in /shop page.

--- src/components/templates/shop/FiltersSlider/FiltersSlider.jsx
+++ src/components/templates/shop/FiltersSlider/FiltersSlider.jsx
@@ -7,13 +7,11 @@ import CategoriesDrawer from '@modules/Drawers/CategoriesDrawer/CategoriesDrawer
 import FiltersDrawer from '@modules/Drawers/FiltersDrawer/FiltersDrawer';
 import PriceRangeSlider from '@modules/PriceRangeSlider/PriceRangeSlider';
 
-const filters = [{ title: 'مدت زمان اشتراک' }, { title: 'نحوه فعال سازی' }, { title: 'زبان ها' }];
-
 export default function FiltersSlider({ categories, priceRange }) {
   return (
     <div className="flex items-center gap-4 min-[880px]:hidden">
       <span className="text-nowrap">فیلتر ها :‌</span>
-      <Swiper modules={[FreeMode]} slidesPerView="auto" freeMode>
+      <Swiper modules={[FreeMode]} slidesPerView="auto" className="mx-0!" freeMode>
         <SwiperSlide className="w-auto!">
           <CategoriesDrawer categories={categories} />
         </SwiperSlide>
@@ -23,13 +21,6 @@ export default function FiltersSlider({ categories, priceRange }) {
             <PriceRangeSlider priceRange={priceRange} />
           </FiltersDrawer>
         </SwiperSlide>
-        {filters.map((filter) => (
-          <SwiperSlide key={filter.title} className="w-auto!">
-            <button className="bg-foreground hover:bg-primary hover:text-box mx-1.5 rounded-2xl rounded-tr-lg px-3 py-1.5 text-[#b7b7b7] transition-colors duration-300">
-              {filter.title}
-            </button>
-          </SwiperSlide>
-        ))}
       </Swiper>
     </div>
   );
