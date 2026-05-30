6acb27272ba1b95b30d69a0aea03b8910aa3e7f8
Nima Zamani <zamani.nima18@gmail.com>
2026-05-30 18:54:40 +0330

    fix: adjusted header particles in dashboard.

--- src/app/(dashboard)/layout.jsx
+++ src/app/(dashboard)/layout.jsx
@@ -39,8 +39,8 @@ export default function layout({ children }) {
           </div>
         </div>
         <div className="relative hidden py-10 lg:block lg:pt-12.5">
-          <Particle className="-top-35 left-0 size-64 opacity-35 blur-[70px]" />
-          <Particle className="top-5 right-0 hidden size-50 opacity-50 blur-[80px] lg:block" />
+          <Particle className="-top-35 -left-10 size-64 opacity-35 blur-[70px]" />
+          <Particle className="top-0 -right-30 hidden size-50 opacity-50 blur-[80px] lg:block" />
           <DesktopHeader />
         </div>
       </header>
